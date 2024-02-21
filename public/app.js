d2eedce0483d336f6d7bd42939bd8e7e97c50891
MikeAtNavagis <108031794+MikeAtNavagis@users.noreply.github.com>
2024-02-21 11:39:12 -0800

    Backing up firebase

---
+++ public/app.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+import { getAuth } from "firebase/auth";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+
+  
+}
